939a4aa7f099c769a4b6663959cf1ca6ee96b42b
Sha Luo <luosha1015@gmail.com>
2025-04-21 02:08:56 +0100

    initial commit, basic project structure

---
+++ backend/src/main/java/com/shaluo/dbbikes/DbbikesApplication.java
@@ -0,0 +1,13 @@
+package com.shaluo.dbbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbbikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbbikesApplication.class, args);
+	}
+
+}
