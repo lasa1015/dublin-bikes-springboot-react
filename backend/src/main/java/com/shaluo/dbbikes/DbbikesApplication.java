5fa44bc2ced6ef19446915bd8289466a91d43c55
Sha Luo <luosha1015@gmail.com>
2025-04-21 02:16:47 +0100

    feat: backend connected to RDS

--- backend/src/main/java/com/shaluo/dbbikes/DbbikesApplication.java
+++ backend/src/main/java/com/shaluo/dbbikes/DbbikesApplication.java
@@ -1,5 +1,6 @@
 package com.shaluo.dbbikes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DbbikesApplication {
 
 	public static void main(String[] args) {
+
+		// 加载 .env 文件
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// 将变量设置到系统属性中，Spring Boot 才能识别 ${}
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+
 		SpringApplication.run(DbbikesApplication.class, args);
 	}
 
